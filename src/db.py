0f5d2a0e4d10a801eb05a45e66a634e837c4d3af
James Cline <james.cline@gatech.edu>
2012-02-11 16:04:28 -0500

    rough draft table creation code

--- src/db.py
+++ src/db.py
@@ -25,10 +25,67 @@ class DB:
 		if(exists == False):
 			# Indicate there is nothing to load from the DB
 			self.new_DB = True
+			self.cursor.executescript("""
+				create table patients(
+					name,
+					id
+					);
+
+				create table doctors(
+					name,
+					id
+					);
+				
+				create table diagnoses(
+					name,
+					id
+					);
+
+				create table treatments(
+					name,
+					id
+					);
+
+				create table photosets(
+					date,
+					direcotry_hash,
+					diagnosis_txt_hash,
+					physicians_txt_hash,
+					treatment_txt_hash,
+					id,
+					path
+					);
+
+				create table photos(
+					id,
+					path,
+					hash
+					);
+
+				create table configuration(
+					root_dor,
+					new_image_dir,
+					last_user,
+					current_user,
+					db_version
+					);
+
+				create table users(
+					name,
+					id
+					);
+				""")
+
+
+
 		else:
 			# Indicate there are things to load from the DB
 			self.newDB = False
-			
+	
+	def getThumbnails(self, patientID):
+		if(self.newDB):
+			return None
+
 	def exit(self):
 		# Close the DB cursor
 		self.cursor.close()
