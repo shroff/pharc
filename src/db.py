19ed68eced201f8d4642eaa46a761a8108e4e850
James Cline <james.cline@gatech.edu>
2012-02-11 14:37:10 -0500

    fleshing out initializer, added exit()

--- src/db.py
+++ src/db.py
@@ -4,18 +4,32 @@ class DB:
 	"""A sqlite3 wrapper"""
 
 	def __init__(self):
-		exists = True
 
+		# Check if the DB exists before we open it
+		exists = True
 		try:
 			f = open("db.sqlite")
 		except IOError:
 			exists = False;
 
+		# If the DB file already exists, we need to close f
 		if(exists == True):
 			f.close()
 
-		# Open database
+		# Open database or create it if it didn't exist
 		self.connection = sqlite3.connect("db.sqlite")
 		# Create object to make queries to the database
 		self.cursor = self.connection.cursor()
+
+		# Create tables if the DB didn't exist
+		if(exists == False):
+			# Indicate there is nothing to load from the DB
+			self.new_DB = True
+		else:
+			# Indicate there are things to load from the DB
+			self.newDB = False
+			
+	def exit(self):
+		# Close the DB cursor
+		self.cursor.close()
 
