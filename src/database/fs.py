ef9f1f82dfd0d4fa4515f7088a0bc1c7b0083265
James Cline <james.cline@gatech.edu>
2012-02-11 17:55:04 -0500

    stub out class for FS

--- src/database/fs.py
+++ src/database/fs.py
@@ -14,3 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class FS:
+	"""A filesystem manager"""
+
+	def __init__(self):
+		return
