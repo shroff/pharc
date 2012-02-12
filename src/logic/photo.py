512830f97e80b973eb0bc35fc00e07b809dec5cc
Saul Reynolds-Haertle <saulrh@gatech.edu>
2012-02-11 21:38:52 -0500

    Add attributes to photo

--- src/logic/photo.py
+++ src/logic/photo.py
@@ -14,3 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class Photo(object):
+    
+    def __init__(self):
+        pass
