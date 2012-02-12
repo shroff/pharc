b920101f802008c17802a7e500fd812cbcf9d27a
Saul Reynolds-Haertle <saulrh@gatech.edu>
2012-02-11 21:39:13 -0500

    Add attributes to photoset

--- src/logic/photoset.py
+++ src/logic/photoset.py
@@ -14,3 +14,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+class Photoset(object):
+    
+    date = None
+    patient = None # Patient that this photoset belongs to
+    physicians = None # list of Physicians that care about this photoset
+    notes = None # notes about this photoset, string
+    diagnoses = None # list of diagnosis tags attached to this photoset
+    treatments = None # list of treatment tags attached to this photoset
+    uid = None # this photoset's unique identification number, integer
+    photos = None # list of photos in this photoset
+
+    loader = None # PhotosetLoader for this photoset
+    
+    def __init__(self):
+        pass
