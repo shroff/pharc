b7ff496257733ee0b32e34c1deb286134861b0f0
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2012-02-26 07:21:12 -0500

    Added longest-common-subsequence function to Tag

--- src/logic/tags.py
+++ src/logic/tags.py
@@ -77,3 +77,67 @@ class Tag(object):
         query."""
         return query == self.value
 
+    def match_common_subseq(self, query):
+        """Returns the longest common subsequence for use in matching.
+
+        Uses dynamic programming to compute the longest common
+        subsequence between this tag's name and the query. Returns a
+        tuple containing the longest common subsequence itself plus
+        lists of indices indicating where the two strings coincided,
+        in the form (result, [tag name indices], [query indices]). The
+        indices are sorted largest to smallest because that was
+        easier. For example:
+
+        name:  1234
+               ^^^^
+        query: 1224533324
+               ^^   ^   ^
+        result: ("1234", [3,2,1,0], [9,5,1,0])
+
+        name:  testing123testing
+               ^ ^ ^     ^^^^
+        query: thisisatest
+               ^  ^^  ^^^^
+        result: ('tsitest', [13,12,11,10,4,2,0], [10,9,8,7,4,3,0])
+
+        Examples and code stolen from
+        rosettacode.org/wiki/Longest_common_subsequence#Dynamic_Programming_6
+
+        Args:
+            query: the string to match against
+
+        Returns:
+            The compute LCS.
+
+        Raises:
+        """
+
+        result = ""
+        name_index = []
+        query_index = []
+        
+        lengths = \
+            [[0 for j in range(len(query)+1)] for i in range(len(self.value)+1)]
+        # row 0 and column 0 are initialized to 0 already
+        for i, x in enumerate(self.value):
+            for j, y in enumerate(query):
+                if x == y:
+                    lengths[i+1][j+1] = lengths[i][j] + 1
+                else:
+                    lengths[i+1][j+1] = \
+                        max(lengths[i+1][j], lengths[i][j+1])
+        # read the substring out from the matrix
+        x, y = len(self.value), len(query)
+        while x != 0 and y != 0:
+            if lengths[x][y] == lengths[x-1][y]:
+                x -= 1
+            elif lengths[x][y] == lengths[x][y-1]:
+                y -= 1
+            else:
+                assert self.value[x-1] == query[y-1]
+                result = self.value[x-1] + result
+                name_index.append(x-1)
+                query_index.append(y-1)
+                x -= 1
+                y -= 1
+        return (result, name_index, query_index)
