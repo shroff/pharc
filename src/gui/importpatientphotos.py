a860e1ae78ae81c0793cb6afee391212e37e86da
Abhishek Shroff <me@shroffabhishek.net>
2012-04-16 15:08:18 -0400

    Adding patient import view

---
+++ src/gui/importpatientphotos.py
@@ -0,0 +1,49 @@
+# PHARC: a photo archiving application for physicians
+# Copyright (C) 2012 Abhishek Shroff
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+from .importphoto import *
+
+import random
+
+imageBase = "images/"
+imgs = ['caesar.jpg', 'puppy.jpg', 'kitty.jpg', 'punch.jpg']
+
+class ImportPatientPhotos(QWidget):
+  def __init__(self, parent):
+    super(ImportPatientPhotos, self).__init__()
+    self.parent = parent
+
+    self.initUI()
+
+  def initUI(self):
+    vbox = QVBoxLayout()
+
+    for x in xrange(1, 4):
+      hbox = QHBoxLayout()
+      hbox.setAlignment(Qt.AlignCenter)
+      hbox.addWidget(ImportPhoto(self, imageBase + random.choice(imgs)))
+      vbox.addLayout(hbox)
+
+
+    sizeLabel = QLabel("")
+    sizeLabel.setFixedSize(QSize(300, 1))
+    vbox.addWidget(sizeLabel)
+
+    self.setLayout(vbox)
---
+++ src/gui/importphoto.py
@@ -0,0 +1,56 @@
+# PHARC: a photo archiving application for physicians
+# Copyright (C) 2012 Abhishek Shroff
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+class ImportPhoto(QWidget):
+  def __init__(self, parent, path):
+    super(ImportPhoto, self).__init__()
+    self.parent = parent
+    self.imagePath = path
+
+    self.initUI()
+
+  def initUI(self):
+    vbox = QVBoxLayout()
+
+    self.picLabel = QLabel()
+    self.picLabel.setFixedSize(QSize(150, 150))
+
+    image = QImage(self.imagePath)
+    pixmap = QPixmap.fromImage(image)
+    if(not pixmap.isNull()):
+      pixmap = pixmap.scaledToHeight(150)
+      self.picLabel.setPixmap(pixmap)
+      self.picLabel.setFixedSize(pixmap.size())
+
+    self.picLabel.setFrameStyle(QFrame.Panel | QFrame.Box)
+
+    hbox = QHBoxLayout()
+    self.checkbox = QCheckBox(self.imagePath, self)
+    QObject.connect(self.checkbox, SIGNAL('stateChanged(int)'), self.toggle)
+    hbox.setAlignment(Qt.AlignCenter)
+    hbox.addWidget(self.checkbox)
+
+    vbox.addWidget(self.picLabel)
+    vbox.addLayout(hbox)
+
+    self.setLayout(vbox)
+
+  def toggle(arg):
+    print (arg)
