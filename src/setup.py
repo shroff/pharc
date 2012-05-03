732b3a095c27f30060ff2dfba7d201569308d457
Michael Polson <mdpolson@gatech.edu>
2012-05-02 22:07:39 -0400

    Allowed for .dll imports when creating the .exe in the setup file.  The path to the directory must be modified to reflect your install directory though.

--- src/setup.py
+++ src/setup.py
@@ -1,11 +1,22 @@
 import sys
 from distutils.core import setup
 
+#Modify this to be your own directory - or find a way to fetch the path to it
+PyQt4imageformatDir = 'A:/Program Files/Python2/Lib/site-packages/PyQt4/plugins/imageformats/'
+
 if 'py2exe' in sys.argv:
     import py2exe
 
-    kwargs = dict(windows=[{'script':'pharc.py', 'dest_base': 'gui'}],
-                  options={'py2exe': dict(includes=["sip"], optimize=2, compressed=1)})
+    kwargs = dict(console=[{'script':'pharc.py', 'dest_base': 'gui'}],
+                  options={'py2exe': dict(includes=["sip"], optimize=2, compressed=1)},
+                  data_files=[('imageformats', [
+                      PyQt4imageformatDir + 'qgif4.dll',
+                      PyQt4imageformatDir + 'qjpeg4.dll',
+                      PyQt4imageformatDir + 'qtga4.dll',
+                      PyQt4imageformatDir + 'qtiff4.dll',
+                      PyQt4imageformatDir + 'qico4.dll',
+                      PyQt4imageformatDir + 'qmng4.dll',
+                      PyQt4imageformatDir + 'qsvg4.dll'])])
 
 else:
     kwargs = dict(windows=['pharc.py'])
